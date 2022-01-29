9e868b239ca429ee46bd7dc023b17ae218543ea2
Adam Czajkowski <a.c.czajkowski@gmail.com>
2022-01-29 13:40:00 +0100

    plot3d: poor grid => nicer pointgs

--- plot3d.cpp
+++ plot3d.cpp
@@ -17,8 +17,7 @@ static const float SQRT_MAX_FFT_OUTPUT = sqrt(MAX_FFT_OUTPUT);
 static GLuint program;
 static GLint attribute_coord3d;
 static GLint uniform_vertex_transform;
-
-static GLuint vbo[2];
+static GLuint vbo;
 
 static struct {
   const float r = 3;
@@ -55,10 +54,8 @@ void plot3dDisplay(const std::vector<double> &fftLabels,
   const size_t M = fftValues.size();
   const double labelSpan = span(fftLabels.data(), fftLabels.size());
 
-  // Create two vertex buffer objects
-  glGenBuffers(2, vbo);
+  glGenBuffers(1, &vbo);
 
-  // Create an array for 101 * 101 vertices
   glm::vec3 vertices[N][M];
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < M; j++) {
@@ -69,33 +66,8 @@ void plot3dDisplay(const std::vector<double> &fftLabels,
     }
   }
 
-  // Tell OpenGL to copy our array to the buffer objects
-  glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices, GL_STATIC_DRAW);
-
-  // Create an array of indices into the vertex array that traces both
-  // horizontal and vertical lines
-  const size_t num_indices = 2 * (M * (N - 1) + N * (M - 1));
-  GLushort indices[num_indices];
-  int idx = 0;
-
-  for (int i = 0; i < M - 1; i++) {
-    for (int j = 0; j < N; j++) {
-      indices[idx++] = i * N + j;
-      indices[idx++] = i * N + j + 1;
-    }
-  }
-
-  for (int i = 0; i < N - 1; i++) {
-    for (int j = 0; j < M; j++) {
-      indices[idx++] = i * M + j;
-      indices[idx++] = (i + 1) * M + j;
-    }
-  }
-
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[1]);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof indices, indices,
-               GL_STATIC_DRAW);
 
   glUseProgram(program);
 
@@ -107,15 +79,13 @@ void plot3dDisplay(const std::vector<double> &fftLabels,
   glUniformMatrix4fv(uniform_vertex_transform, 1, GL_FALSE,
                      glm::value_ptr(vertex_transform));
 
-  /* Draw the grid using the indices to our vertices using our vertex buffer
-   * objects */
   glEnableVertexAttribArray(attribute_coord3d);
 
-  glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
   glVertexAttribPointer(attribute_coord3d, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[1]);
-  glDrawElements(GL_LINES, num_indices, GL_UNSIGNED_SHORT, 0);
+  glPointSize(4);
+  glDrawArrays(GL_POINTS, vbo, M * N);
 
   /* Stop using the vertex buffer object */
   glDisableVertexAttribArray(attribute_coord3d);
